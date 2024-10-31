5db1cd3263b20d27922880c446feda552c120e9f
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-10-31 14:22:22 +0530

    feat:(component): about, social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [tailwind({applyBaseStyles:false})]
+  integrations: [tailwind({applyBaseStyles:false}), icon()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary:'#fabd2f'
+			},
+			fontFamily:{
+				sans:['Poppins']
+			}
+		},
 	},
 	plugins: [],
 }
