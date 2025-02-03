b4b3441b214f329952d4e82b9ddf099359f2dcc2
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-02-03 22:52:17 +0530

    feat(ui): add projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	"darkMode":"class",
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				primary:'#fabd2f',
-				muted:'#e8e8e8'
-			},
-			fontFamily:{
-				sans:['Poppins']
-			}
-		},
-	},
-	plugins: [ require('@tailwindcss/typography'),],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fabd2f",
+        muted: "#e8e8e8",
+      },
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
